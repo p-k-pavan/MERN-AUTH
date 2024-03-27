dc43977b9cfe26d25852e57beb171a486199d6ca
p-k-pavan <pkpavan2003@email.com>
2024-03-27 13:47:59 +0530

    Complete sign up page functionality

--- api/models/user.models.js
+++ api/models/user.models.js
@@ -1,32 +1,24 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema(
-  {
-    username: {
-      type: String,
+const { Schema, model } = mongoose;
 
-      required: true,
-
-      unique: true,
-    },
-
-    email: {
-      type: String,
-
-      required: true,
-
-      unique: true,
-    },
-
-    password: {
-      type: String,
-
-      required: true,
-    },
+const userSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true
   },
-  { timestamps: true }
-);
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+}, { timestamps: true });
 
-const User = mongoose.model("User", userSchema);
+const User = model("User", userSchema);
 
 export default User;
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,17 +1,82 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import{ useState }from "react";
+import { Link } from "react-router-dom";
+
 
 export default function () {
+const [formData, setFormData]= useState({});
+const [error,setError]=useState(null);
+const [loading,setLoading]=useState(false);
+const handleChange = (e)=>{
+  setFormData({...formData,[e.target.id]:e.target.value})
+}
+const handleSubmit = async(e)=>{
+   e.preventDefault();
+   try{
+    setLoading(true)
+    setError(false)
+    const res = await fetch("/api/auth/signup",
+    {
+       method:"POST",
+       headers:{
+         'Content-Type':'application/json'
+       },
+       body:JSON.stringify(formData),
+
+    });
+    const data =await res.json();
+    
+    setLoading(false);
+    if(data.success===false){
+      setError(true)
+      return;
+    }
+    
+
+   }catch(err){
+    setLoading(false);
+    setError(true)
+   }
+}
+
   return (
     <div>
       <h1 className="text-3xl font-semibold text-center my-7">SIGN UP</h1>
-      <form className="flex flex-col max-w-lg mx-auto gap-4">
-      <input type="text" placeholder="UserName" className="p-3 focus:outline-slate-600 bg-slate-200 rounded-lg" id="username"/>
-      <input type="email" placeholder="Email" className="p-3 focus:outline-slate-600 bg-slate-200 rounded-lg" id="email"/>
-      <input type="password" placeholder="Password" className="p-3 focus:outline-slate-600 bg-slate-200 rounded-lg" id="password"/>
-      <button className="bg-slate-600 text-white font-semibold p-3 rounded-lg hover:bg-slate-500" >SIGNUP</button>
-        </form>
-        <p className="text-center my-7 font-semibold"> Alredy have accout? <Link to='/sign-in' className="text-blue-500">Sign-In</Link></p>
+      <form className="flex flex-col max-w-lg mx-auto gap-4" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="UserName"
+          className="p-3 focus:outline-slate-600 bg-slate-200 rounded-lg"
+          id="username"
+          onChange={handleChange}
+        />
+        <input
+          type="email"
+          placeholder="Email"
+          className="p-3 focus:outline-slate-600 bg-slate-200 rounded-lg"
+          id="email"
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          className="p-3 focus:outline-slate-600 bg-slate-200 rounded-lg"
+          id="password"
+          onChange={handleChange}
+        />
+        <button className="bg-slate-600 text-white font-semibold p-3 rounded-lg hover:bg-slate-500">
+          {loading? "Loading...":"SIGNUP"}
+        </button>
+      </form>
+      <p className="text-center my-7 font-semibold">
+        {" "}
+        Alredy have accout?{" "}
+        <Link to="/sign-in" className="text-blue-500">
+          Sign-In
+        </Link>
+      </p>
+
+      <p className="text-red-500 text-center">{error && "something went worng"}</p>
     </div>
-  )
+  );
 }
+
